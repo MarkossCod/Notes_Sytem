b0f25e85fda92602f17a1c69e25358aa82af4768
Markinhos007 <markitosstop2000@gmail.com>
2026-05-29 22:12:22 -0300

    force https absolutely

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -10,8 +10,13 @@ class AppServiceProvider extends ServiceProvider
     }
     public function boot(): void
     {
-        if (config('app.env') === 'production') {
-            URL::forceScheme('https');
-        }
+        URL::forceScheme('https');
+        \Illuminate\Support\Facades\Request::setTrustedProxies(
+            ['REMOTE_ADDR'],
+            \Illuminate\Http\Request::HEADER_X_FORWARDED_FOR |
+            \Illuminate\Http\Request::HEADER_X_FORWARDED_HOST |
+            \Illuminate\Http\Request::HEADER_X_FORWARDED_PORT |
+            \Illuminate\Http\Request::HEADER_X_FORWARDED_PROTO
+        );
     }
 }
