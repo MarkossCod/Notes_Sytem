a5f55da18a71b40d0c0a90b656cba0d49ef3f692
Markinhos007 <markitosstop2000@gmail.com>
2026-05-31 17:30:46 -0300

    fix sobre modal

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -42,16 +42,13 @@
 </div>
 
 {{-- MODAL SOBRE --}}
-<div id="sobreModal" class="modal-overlay" onclick="closeSobreModal()" style="display:none;">
+<div id="sobreModal" class="modal-overlay" onclick="closeSobreModal()">
     <div class="modal-box sobre-box" onclick="event.stopPropagation()">
-
         <div class="modal-header">
             <h2>👤 Sobre o Sistema</h2>
             <button class="modal-close" onclick="closeSobreModal()">✕</button>
         </div>
-
         <div class="sobre-content">
-
             <div class="sobre-item">
                 <span class="sobre-icon">🧑‍💻</span>
                 <div>
@@ -59,7 +56,6 @@
                     <p class="sobre-value">Markos Samuell</p>
                 </div>
             </div>
-
             <div class="sobre-item">
                 <span class="sobre-icon">🏫</span>
                 <div>
@@ -67,7 +63,6 @@
                     <p class="sobre-value">SENAI-CTTI-MG</p>
                 </div>
             </div>
-
             <div class="sobre-item">
                 <span class="sobre-icon">💡</span>
                 <div>
@@ -75,9 +70,7 @@
                     <p class="sobre-value">Sistema desenvolvido para organizar e gerenciar chamados de forma prática, permitindo criar notas e dividi-las em seções para melhor controle das atividades.</p>
                 </div>
             </div>
-
         </div>
-
     </div>
 </div>
 
